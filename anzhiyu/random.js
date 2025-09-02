52fcd92db90a090f74910079b81285fcf13f24be
iiab-0903 <1410245679@qq.com>
2025-09-02 18:59:31 +0800

    Site updated: 2025-09-02 18:59:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/28/hello-world/","2025/07/19/HIVE/","2025/07/19/软件工程/","2025/07/16/图灵竞赛系统/"];function toRandomPost(){
+var posts=["2025/04/28/hello-world/","2025/07/16/图灵竞赛系统/","2025/07/19/HIVE/","2025/07/19/软件工程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
