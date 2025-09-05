c522a9da40cddd91c25ece946483fac5621f0a09
iiab-0903 <1410245679@qq.com>
2025-09-05 11:44:42 +0800

    Site updated: 2025-09-05 11:44:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/19/HIVE/","2025/07/16/图灵竞赛系统/","2025/04/28/hello-world/","2025/07/19/软件工程/","2025/07/19/廉政管理系统/","2025/09/02/廉政系统项目计划书/"];function toRandomPost(){
+var posts=["2025/04/28/hello-world/","2025/07/19/软件工程/","2025/09/05/海洋牧场/海洋牧场/","2025/07/19/HIVE/","2025/07/16/图灵竞赛系统/","2025/07/19/廉政管理系统/廉政管理系统/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
